0bded1d3ccf94b23155637d262710b2dfa93e2ae
Marron Akanishi <marron.general@gmail.com>
2017-04-28 22:58:56 +0900

    エラー対策

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 chrome.commands.onCommand.addListener(function(command){
     if(command == "play-pause"){
         chrome.tabs.query( {url: "https://www.youtube.com/watch?*"}, function(tabs){
-            chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-play-button")[0].click()'})
+            if(tabs.length != 0)chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-play-button")[0].click()'})
         })
     }
     if(command == "prev"){
         chrome.tabs.query( {url: "https://www.youtube.com/watch?*list=*"}, function(tabs){
-            chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-prev-button")[0].click()'})
+             if(tabs.length != 0)chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-prev-button")[0].click()'})
         })
     }
     if(command == "next"){
         chrome.tabs.query( {url: "https://www.youtube.com/watch?*list=*"}, function(tabs){
-            chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-next-button")[0].click()'})
+             if(tabs.length != 0)chrome.tabs.executeScript(tabs[0].id,{code: 'document.getElementsByClassName("ytp-next-button")[0].click()'})
         })
     }
 })
